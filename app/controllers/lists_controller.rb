460d052d6ee2f9beaff1cffc40517db08a09186f
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-04 01:25:24 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,25 +3,28 @@ class ListsController < ApplicationController
     #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     #1＆2 データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
     #3データをデータベースに保存するためのsaveメソッド実行
     list.save
-    #4トップ画面へリダイレクト
-    redirect_to '/top'
+    #詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    #今回はレコードを1件だけ取得するので、インスタンス変数名は単数形の「@list」にします。
+    @list = List.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
   #ストロングパラメータ
   def list_params
