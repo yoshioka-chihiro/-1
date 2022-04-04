7dd676d11bfc3320eddc002cdb9d18e63fe00614
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-04 05:52:20 +0000

    [Finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,11 +6,14 @@ class ListsController < ApplicationController
 
   def create
     #1＆2 データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     #3データをデータベースに保存するためのsaveメソッド実行
-    list.save
+    if @list.save
     #詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+     redirect_to list_path(list.id)
+   else
+     render :new
+   end
   end
 
   def index
